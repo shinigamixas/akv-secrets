dcd4603f85b0c855d842b8d51984e1bceccc4201
Oleksii Kostin <oleksii.kostin@empeek.com>
2021-04-24 13:54:49 +0300

    Renamed files, removed not used usings

--- ShotgunCodeAkvSecretsNET/Controllers/SecretController.cs
+++ ShotgunCodeAkvSecretsNET/Controllers/SecretController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
+﻿using System.Net;
 using System.Net.Http.Formatting;
 using System.Threading.Tasks;
 using System.Web.Http;
